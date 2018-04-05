1dc8d120c67784e667912daeb7f1d5fb19f2ea3c
Death <DeathGameDev@gmail.com>
2018-04-05 13:42:14 -0500

    Removed unused code in BoundsDisplay

--- Assets/Code/Displays/Handle/BoundsHandleDisplay.cs
+++ Assets/Code/Displays/Handle/BoundsHandleDisplay.cs
@@ -34,18 +34,12 @@ namespace SVHandles.Displays.Handle
             // Draw our handles for dragging scale.
             Vector3 outSize = outBounds.size;
 
-
-            BoxCollider b;
-
-            float xSideScale = (outSize.z * outSize.y) / 2f;
             outSize.x = Handles.ScaleSlider(outSize.x, center + (Vector3.right * outSize.x / 2f), Vector3.right, Quaternion.LookRotation(Vector3.right), HandleUtility.GetHandleSize(center + (Vector3.right * outSize.x / 2f)), 1f);
             outSize.x = Handles.ScaleSlider(outSize.x, center + (Vector3.left * outSize.x / 2f), Vector3.left, Quaternion.LookRotation(Vector3.left), HandleUtility.GetHandleSize(center + (Vector3.left * outSize.x / 2f)), 1f);
 
-            float ySideScale = (outSize.x * outSize.z) / 2f;
             outSize.y = Handles.ScaleSlider(outSize.y, center + (Vector3.up * outSize.y / 2f), Vector3.up, Quaternion.LookRotation(Vector3.up), HandleUtility.GetHandleSize(center + (Vector3.up * outSize.y / 2f)), 1f);
             outSize.y = Handles.ScaleSlider(outSize.y, center + (Vector3.down * outSize.y / 2f), Vector3.down, Quaternion.LookRotation(Vector3.down), HandleUtility.GetHandleSize(center + (Vector3.down * outSize.y / 2f)), 1f);
 
-            float zSideScale = (outSize.x * outSize.y) / 2f;
             outSize.z = Handles.ScaleSlider(outSize.z, center + (Vector3.forward * outSize.z / 2f), Vector3.forward, Quaternion.LookRotation(Vector3.forward), HandleUtility.GetHandleSize(center + (Vector3.forward * outSize.z / 2f)), 1f);
             outSize.z = Handles.ScaleSlider(outSize.z, center + (Vector3.back * outSize.z / 2f), Vector3.back, Quaternion.LookRotation(Vector3.back), HandleUtility.GetHandleSize(center + (Vector3.back * outSize.z / 2f)), 1f);
 
